8fb314aebd1ad93eae17f4e275aa810d6e1c5390
mbrionesz <matias.briones95@gmail.com>
2023-12-21 22:35:01 -0300

    Initial files

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,10 @@
+function example(a, b, c){
+    return a+b+c
+    }
+
+// Transformar a declaración de expresión
+
+let example = function(a, b, c) {
+    return a + b + c;
+  };
+  
---
+++ assets/js/2_arrow.js
@@ -0,0 +1,7 @@
+let suma = function(a, b){
+    return a + b
+    }
+
+// Transformación a arrow function
+
+suma = (a, b) => a + b;
