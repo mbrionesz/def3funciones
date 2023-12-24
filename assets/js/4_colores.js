5b8d2351f432e882942bfa6eec5b16234436392d
mbrionesz <matias.briones95@gmail.com>
2023-12-24 00:26:06 -0300

    Final changes

--- assets/js/4_colores.js
+++ assets/js/4_colores.js
@@ -1,6 +1,3 @@
-// Variable global para almacenar el color
-let globalColor = '';
-
 // Asignar el evento clic a los divs azul, rojo, verde, amarillo
 document.getElementById('azul').addEventListener('click', changeColorOnClick);
 document.getElementById('rojo').addEventListener('click', changeColorOnClick);
@@ -12,6 +9,24 @@ function changeColorOnClick(event) {
   event.target.style.backgroundColor = 'black';
 }
 
+// Función para obtener el color asociado a una tecla
+function getColorForKey(key) {
+    switch (key) {
+      case 'a':
+        return 'pink';
+      case 's':
+        return 'orange';
+      case 'd':
+        return '#51D1F6';
+      case 'q':
+        return 'purple';
+      case 'w':
+        return 'gray';
+      case 'e':
+        return '#804000';
+    }
+  }
+
 // Función para cambiar el color del div "key" según la tecla presionada
 function changeColorByKey(event) {
   globalColor = getColorForKey(event.key);
@@ -20,36 +35,12 @@ function changeColorByKey(event) {
 
 // Función para crear un nuevo div con colores específicos
 function createNewDiv(color) {
-  const newDiv = document.createElement('div');
-  newDiv.style.width = '200px';
-  newDiv.style.height = '200px';
-  newDiv.style.backgroundColor = color;
-  document.body.appendChild(newDiv);
-
-  // Cambiar el color del div "key" al color del nuevo div
-  globalColor = color;
-  document.getElementById('key').style.backgroundColor = globalColor;
-}
-
-// Función para obtener el color asociado a una tecla
-function getColorForKey(key) {
-  switch (key) {
-    case 'a':
-      return 'pink';
-    case 's':
-      return 'orange';
-    case 'd':
-      return '#51D1F6';
-    case 'q':
-      return 'purple';
-    case 'w':
-      return 'gray';
-    case 'e':
-      return '#804000';
-    default:
-      return '';
+    const newDiv = document.createElement('div');
+    newDiv.style.width = '200px';
+    newDiv.style.height = '200px';
+    newDiv.style.backgroundColor = color;
+    document.body.appendChild(newDiv);
   }
-}
 
 // Asignar el evento keydown a toda la página para cambiar el color del div "key"
 document.addEventListener('keydown', function (event) {
